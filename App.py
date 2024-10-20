00672a7a8632e268ae93f9bfa8f3c33cc4b353aa
Selsa bil <120721447+selsabilbouchama@users.noreply.github.com>
2024-10-20 19:29:04 +0100

    Update App.py

--- App.py
+++ App.py
@@ -1,1 +1,6 @@
+@app.route('/add/<int:numberA>/<int:numberB>', methods=['GET'])
+def add(numberA, numberB):
+    result = numberA + numberB
+    return jsonify(status=200, result=result)
 
+@app.route('/minus/<int:numberA>/<int:numberB>', methods=['GET'])
