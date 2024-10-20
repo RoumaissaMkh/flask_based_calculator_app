eca57ceba2e4c2f2db111c7eabc6fcd21120e65d
Roumaissa Mekhoukh <146542059+RoumaissaMkh@users.noreply.github.com>
2024-10-20 18:52:44 +0100

    Update App.py
    the multiplication operation

--- App.py
+++ App.py
@@ -1,1 +1,41 @@
 
+
+
+
+
+
+
+@app.route('/multiply/<int:numberA>/<int:numberB>', methods=['GET'])
+def multiply(numberA, numberB):
+    result = numberA * numberB
+    return jsonify(status=200, result=result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
