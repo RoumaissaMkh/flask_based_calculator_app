b18bf4c96ca5b8bc1f6226e99016310978b5816a
ghedfa <146786283+ghedfa@users.noreply.github.com>
2024-10-20 20:04:56 +0200

    Update App.py

--- App.py
+++ App.py
@@ -1,1 +1,6 @@
-
+@app.route('/divide/<int:numberA>/<int:numberB>', methods=['GET'])
+def divide(numberA, numberB):
+    if numberB == 0:
+        return jsonify(status=400, result='Division by zero error')
+    result = numberA / numberB
+    return jsonify(status=200, result=result)
